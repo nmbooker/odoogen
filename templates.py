7098e3249935aaeaca727785c52d35c12748c9b7
Nick Booker <nick.booker@opusvl.com>
2017-12-01 12:14:20 +0000

    Wrap the IPA schedule text to within the hash rulers

--- templates.py
+++ templates.py
@@ -43,7 +43,8 @@ You should have received a copy of the GNU Affero General Public License
 along with this program. If not, see <http://www.gnu.org/licenses/>.
 """,
     'IPA-Schedule': """
-This software is licensed according to the "IP Assignment Schedule" provided with the development project.
+This software is licensed according to the "IP Assignment Schedule" provided
+with the development project.
 """,
 }
 
